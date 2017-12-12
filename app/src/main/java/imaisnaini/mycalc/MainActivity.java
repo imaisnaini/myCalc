44815e30a6016f49a4921bbb6153d67757d7c6d1
imaisnaini <imaisnaini@gmail.com>
2017-12-12 14:54:19 +0700

    - Rombak total xml  - Hapus isi java

--- app/src/main/java/imaisnaini/mycalc/MainActivity.java
+++ app/src/main/java/imaisnaini/mycalc/MainActivity.java
@@ -2,12 +2,19 @@ package imaisnaini.mycalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
 }
